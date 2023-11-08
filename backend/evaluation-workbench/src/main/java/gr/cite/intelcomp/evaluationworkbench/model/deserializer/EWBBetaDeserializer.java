9dd915ee5b41014db9cdb32381a1eacf80f3bf59
George Kalampokis <gkalampokis@cite.gr>
2023-11-08 18:58:56 +0200

    Fix Deserializer issue

--- backend/evaluation-workbench/src/main/java/gr/cite/intelcomp/evaluationworkbench/model/deserializer/EWBBetaDeserializer.java
+++ backend/evaluation-workbench/src/main/java/gr/cite/intelcomp/evaluationworkbench/model/deserializer/EWBBetaDeserializer.java
@@ -2,11 +2,9 @@ package gr.cite.intelcomp.evaluationworkbench.model.deserializer;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.type.CollectionType;
 import gr.cite.intelcomp.evaluationworkbench.model.EWBTopicBeta;
 
 import java.io.IOException;
@@ -15,6 +13,11 @@ import java.util.List;
 public class EWBBetaDeserializer extends JsonDeserializer<List<EWBTopicBeta>> {
     @Override
     public List<EWBTopicBeta> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        return EWBTopicBeta.mapToList(p.getValueAsString());
+        String value = p.getValueAsString();
+        if (value == null) {
+            value = p.readValuesAs(new TypeReference<List<String>>() {
+            }).next().get(0);
+        }
+        return EWBTopicBeta.mapToList(value);
     }
 }
